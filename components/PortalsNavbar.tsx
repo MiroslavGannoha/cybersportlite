83e6a38f0e2666f85450a507b634e07b955d651e
MiroslavGannoha <miroslav.gannoha@gmail.com>
2020-08-06 18:16:55 +0300

    Navbar links fix

--- components/PortalsNavbar.tsx
+++ components/PortalsNavbar.tsx
@@ -1,45 +1,53 @@
 import * as React from 'react';
 import styles from '../styles/index.module.css';
 import { portalsDomains } from '../settings';
-import Link from 'next/link'
+import Link from 'next/link';
 
 const PortalsNavbar = () => {
     return (
         <ul className={styles.list}>
             <li>
                 <Link href="/">
-                    <img
-                        width="40px"
-                        alt={portalsDomains.cybersport}
-                        src={`${portalsDomains.cybersport}/favicon.ico`}
-                    />
+                    <a>
+                        <img
+                            width="40px"
+                            alt={portalsDomains.cybersport}
+                            src={`${portalsDomains.cybersport}/favicon.ico`}
+                        />
+                    </a>
                 </Link>
             </li>
             <li>
                 <Link href="/gameinside">
-                    <img
-                        width="40px"
-                        alt={portalsDomains.gameInside}
-                        src={`${portalsDomains.gameInside}/favicon.ico`}
-                    />
+                    <a>
+                        <img
+                            width="40px"
+                            alt={portalsDomains.gameInside}
+                            src={`${portalsDomains.gameInside}/favicon.ico`}
+                        />
+                    </a>
                 </Link>
             </li>
             <li>
                 <Link href="/">
-                    <img
-                        width="40px"
-                        alt={portalsDomains.cyberSports}
-                        src={`${portalsDomains.cyberSports}/favicon.ico`}
-                    />
+                    <a>
+                        <img
+                            width="40px"
+                            alt={portalsDomains.cyberSports}
+                            src={`${portalsDomains.cyberSports}/favicon.ico`}
+                        />
+                    </a>
                 </Link>
             </li>
             <li>
                 <Link href="/">
-                    <img
-                        width="40px"
-                        alt={portalsDomains.championat}
-                        src={`${portalsDomains.championat}/favicon.ico`}
-                    />
+                    <a>
+                        <img
+                            width="40px"
+                            alt={portalsDomains.championat}
+                            src={`${portalsDomains.championat}/favicon.ico`}
+                        />
+                    </a>
                 </Link>
             </li>
         </ul>
