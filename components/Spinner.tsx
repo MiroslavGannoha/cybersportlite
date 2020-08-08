2390199a2a79de84772c8387484cb4eea87a7d72
MiroslavGannoha <miroslav.gannoha@gmail.com>
2020-08-08 17:54:31 +0300

    spinner added

---
+++ components/Spinner.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import styles from '../styles/spinner.module.css';
+
+const Spinner = () => {
+    return (
+        <div className={styles.spinner}>
+        </div>
+    );
+};
+
+export default Spinner;
