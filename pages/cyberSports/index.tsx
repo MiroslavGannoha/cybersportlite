114f0a9c79686b35eb85b6afee3b968ea9a6801c
MiroslavGannoha <miroslav.gannoha@gmail.com>
2020-08-10 18:22:51 +0300

    fixes

---
+++ pages/cyberSports/index.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import PortalsNavbar from '../../components/PortalsNavbar';
+import styles from '../../styles/index.module.css';
+
+const CyberSportsNews = () => {
+
+    return (
+        <div className={styles.container}>
+            <PortalsNavbar />
+            CyberSports
+        </div>
+    );
+};
+
+export default CyberSportsNews;
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,12 +5,14 @@ import Head from 'next/head';
 import PageHead from '../components/PageHead';
 
 const MainPage = ({ news }: { news: any[] }) => {
-
     return (
         <div className={styles.container}>
             <PageHead />
             <PortalsNavbar />
-            Киберспортивные новости с популярных порталов оптимизированные под мобильные устройства.
+            <h2>
+                Киберспортивные новости с популярных порталов оптимизированные
+                под мобильные устройства.
+            </h2>
         </div>
     );
 };
